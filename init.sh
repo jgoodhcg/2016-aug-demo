8e7d6a9e422fa30a3aa289eab142cf1b9d3a8725
jgoodhcg <jgoodhcg@gmail.com>
2016-08-30 19:00:48 -0400

    simple react vagrant dev env
    webpack dev server with hot reloading working
    run npm run-script dev
    nginx also installed
    symlink files or setup config

---
+++ init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y nginx
+sudo apt-get install -y nodejs
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+sudo apt-get install -y npm
+cd /home/vagrant/2016-aug-demo/
+rm -rf node_modules && npm install
+
+
+
