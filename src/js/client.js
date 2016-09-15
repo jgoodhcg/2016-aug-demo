7bf56c4ea6518a740da2cdb7e3e642e2412d1fdb
jgoodhcg <jgoodhcg@gmail.com>
2016-09-15 09:59:04 -0400

    displaying the last thirty seconds

--- src/js/client.js
+++ src/js/client.js
@@ -3,7 +3,7 @@ var d3 = require("d3");
 var data = [],
     cursorX = 0, cursorY= 0,
     prevX = 0, prevY = 0,
-    n = 40;
+    sec = 30;
 
 var svg = d3.select("svg"),
     margin = {top: 20, right: 20, bottom: 20, left: 40},
@@ -12,16 +12,16 @@ var svg = d3.select("svg"),
     g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 var x = d3.scaleLinear()
-    .domain([0, n - 1])
+    .domain([0, sec*1000])
     .range([0, width]);
 
 var y = d3.scaleLinear()
-    .domain([0, 20])
+    .domain([0, 60])
     .range([height, 0]);
 
 var line = d3.line()
-    .x(function(d, i) { return x(i); })
-    .y(function(d, i) { return y(d); });
+    .x(function(d, i) { return x(Date.now() - d.time); })
+    .y(function(d, i) { return y(d.distance); });
 
 g.append("defs").append("clipPath")
     .attr("id", "clip")
@@ -48,22 +48,24 @@ g.append("g")
     .ease(d3.easeLinear);
 
 function tick() {
-    var d = Math.sqrt(Math.pow(prevX - cursorX, 2) +
+    var distance = Math.sqrt(Math.pow(prevX - cursorX, 2) +
                       Math.pow(prevY - cursorY, 2));
 
     prevX = cursorX;
     prevY = cursorY;
 
-    console.log(d);
-    data.push(d);
+    data.push({
+        "distance": distance,
+        "time" : Date.now()
+    });
 
     d3.select(".line")
         .attr("d", line)
         .attr("transform", null);
 
     // Pop the old data point off the front.
-    if(data.length > n){
-        data.shift();
+    if(data.length > sec){
+        // data.shift();
     }
 
 }
