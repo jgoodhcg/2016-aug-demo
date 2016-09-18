348ca02012e011324ffec82dacdf9244d8873bd5
jgoodhcg <jgoodhcg@gmail.com>
2016-09-18 12:36:45 -0400

    refactored into es6 class

---
+++ src/js/LiveGraph.js
@@ -0,0 +1,77 @@
+var d3 = require("d3");
+
+export default class LiveGraph{
+    constructor(args){
+        this.data = [];
+        this.svg_id = args.svg_id;
+        this.time_window = args.time_window;
+
+        this.svg = d3.select("#"+this.svg_id);
+        var  width = +this.svg.attr("width") - args.margin.left - args.margin.right,
+            height = +this.svg.attr("height") - args.margin.top - args.margin.bottom,
+            g = this.svg.append("g").attr("transform", "translate(" + args.margin.left + "," + args.margin.top + ")");
+
+        var x = d3.scaleLinear()
+            .domain([0, this.time_window])
+            .range([0, width]);
+
+        var y = d3.scaleLinear()
+            .domain([0, args.max_distance])
+            .range([height, 0]);
+
+        this.line = d3.line()
+            .x(function(d, i) { return x((Date.now() - d.time)); })
+            .y(function(d, i) { return y(d.distance); });
+
+        g.append("defs").append("clipPath")
+            .attr("id", "clip")
+            .append("rect")
+            .attr("width", width)
+            .attr("height", height);
+
+        g.append("g")
+            .attr("class", "axis axis--x")
+            .attr("transform", "translate(0," + y(0) + ")")
+            .call(d3.axisBottom(x));
+
+        g.append("g")
+            .attr("class", "axis axis--y")
+            .call(d3.axisLeft(y));
+
+        g.append("g")
+            .attr("clip-path", "url(#clip)")
+            .append("path")
+            .datum(this.data)
+            .attr("class", this.svg_id+"_line");
+            .transition()
+            .duration(500)
+            .ease(d3.easeLinear);
+    }
+
+    render(args){
+        var distance = Math.sqrt(Math.pow(args.prevX - args.cursorX, 2) +
+                                 Math.pow(args.prevY - args.cursorY, 2));
+
+        this.data.push({
+            "distance" : distance,
+            "time"     : Date.now(),
+            "velocity" : distance/args.tick_interval
+        });
+
+        d3.select("."+this.svg_id+"_line")
+            .attr("d", this.line)
+            .attr("transform", null);
+
+    }
+
+    filter(){
+        this.data = this.data.filter(function(d){
+            if((Date.now() - d.time) > this.time_window){
+                return false;
+            }else{
+                return true;
+            }
+        }, this);
+    }
+}
+
--- src/js/client.js
+++ src/js/client.js
@@ -1,82 +1,38 @@
-var d3 = require("d3");
+import LiveGraph from "./LiveGraph.js";
 
-var data = [],
-    cursorX = 0, cursorY= 0,
-    prevX = 0, prevY = 0,
-    sec = 30,
+var tick_interval = 5, //milliseconds
     max_distance = Math.sqrt(
         Math.pow(window.innerWidth, 2) + Math.pow(window.innerHeight, 2)
-    );
-
-var svg = d3.select("svg"),
+    ),
     margin = {top: 20, right: 20, bottom: 20, left: 40},
-    width = +svg.attr("width") - margin.left - margin.right,
-    height = +svg.attr("height") - margin.top - margin.bottom,
-    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    cursorX = 0,
+    cursorY = 0,
+    prevX = 0,
+    prevY = 0;
 
-var x = d3.scaleLinear()
-    .domain([0, sec])
-    .range([0, width]);
+var delta_distance = new LiveGraph({
+    "margin"       : margin,
+    "svg_id"       : "delta_distance",
+    "time_window"  : 30000,
+    "max_distance" : max_distance
+});
 
-var y = d3.scaleLinear()
-    .domain([0, max_distance])
-    .range([height, 0]);
+function tick(){
+    delta_distance.render({
+        tick_interval,
+        cursorX,
+        cursorY,
+        prevX,
+        prevY
+    });
 
-var line = d3.line()
-    .x(function(d, i) { return x((Date.now() - d.time)/1000); })
-    .y(function(d, i) { return y(d.distance); });
-
-g.append("defs").append("clipPath")
-    .attr("id", "clip")
-  .append("rect")
-    .attr("width", width)
-    .attr("height", height);
-
-g.append("g")
-    .attr("class", "axis axis--x")
-    .attr("transform", "translate(0," + y(0) + ")")
-    .call(d3.axisBottom(x));
-
-g.append("g")
-    .attr("class", "axis axis--y")
-    .call(d3.axisLeft(y));
-
-g.append("g")
-    .attr("clip-path", "url(#clip)")
-  .append("path")
-    .datum(data)
-    .attr("class", "line")
-  .transition()
-    .duration(500)
-    .ease(d3.easeLinear);
-
-function tick() {
-    var distance = Math.sqrt(Math.pow(prevX - cursorX, 2) +
-                      Math.pow(prevY - cursorY, 2));
+    delta_distance.filter();
 
     prevX = cursorX;
     prevY = cursorY;
-
-    data.push({
-        "distance": distance,
-        "time" : Date.now()
-    });
-
-    d3.select(".line")
-        .attr("d", line)
-        .attr("transform", null);
-
-    data.filter(function(d){
-        if((Date.now() - d.time)/1000 > sec){
-            return false;
-        }else{
-            return true;
-        }
-    });
-
 }
 
-window.setInterval(tick, 5);
+window.setInterval(tick, tick_interval);
 document.onmousemove = function(e){
     cursorX = e.pageX;
     cursorY = e.pageY;
